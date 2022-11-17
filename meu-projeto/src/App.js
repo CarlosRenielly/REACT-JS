8947c4f776243ca2348a9a6b80272d9c54efc92d
Carlos-Renielly <109562348+CarlosRenielly@users.noreply.github.com>
2022-11-17 15:24:22 -0300

    Interpolação {}
    Interpolação

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const name = 'Carlos'
+  
   return (
     <div className="App">
       <h1>Olá Carlos</h1>
       <p>Meu primeiro App</p>
+      <p>Ola, seja bem vindo, {name}</p>
     </div>
   );
 }
