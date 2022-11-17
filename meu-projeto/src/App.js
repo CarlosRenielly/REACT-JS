2edab1d696a734a96f5ac9c5451d660f82145ce7
Carlos-Renielly <109562348+CarlosRenielly@users.noreply.github.com>
2022-11-17 16:34:37 -0300

    Componets
    componets v.1

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './componets/HelloWorld';
 
 function App() {
   
   const name = 'Carlos'
+  const newname = name.toUpperCase()
   
   return (
     <div className="App">
       <h1>Olá Carlos</h1>
       <p>Meu primeiro App</p>
-      <p>Ola, seja bem vindo, {name}</p>
+      <p>Ola, seja bem vindo, {newname}</p>
+      <HelloWorld/>
     </div>
   );
 }
---
+++ meu-projeto/src/componets/Frase.js
@@ -0,0 +1,10 @@
+
+function Frase (){
+  return(
+    <div>
+      <p>Essa é a frase mais linda!</p>
+    </div>
+  )
+}
+
+export default Frase
