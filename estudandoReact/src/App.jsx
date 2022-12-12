d686305fd66bf689a12ed5fd9839b3b30ada8294
Carlos-Renielly <many1458@gmail.com>
2022-12-11 21:08:40 -0300

    LinkQrcode

--- estudandoReact/src/App.jsx
+++ estudandoReact/src/App.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import { useState } from 'react'
+import QRCode from 'react-qr-code'
+import QRCodeLik from 'qrcode'
 import './App.css'
 
-const BemVindo = (props) => {
-  return(
-    <h1>Seja bem vindo {props.nome}</h1>
-  )
-}
 
 
 
 function App() {
+  const [link, setLink] = useState('')
+  const [qrcodeLink, setQrcodeLink] = useState('')
+
+
+
+  function handleGenerate(link_url){  
+    QRCodeLik.toDataURL(link_url, {
+      width: 600,
+      margin: 3,
+    }, function(err, url ){
+      setQrcodeLink(url)
+    })
+  } 
+
+
+
+  function handleQrcode (e){
+    setLink(e.target.value)
+    handleGenerate(e.target.value)
+  }
 
   return (
-    <div className='App'>
-    <BemVindo nome ="Carlos"/>
+    <div className='container'>
+      
+     <QRCode
+      value={link}
+     /> 
+
+      <input className='input'  
+      value={link} 
+      type="text" 
+      placeholder='Digite seu link...' 
+      onChange={(e) => handleQrcode(e)}
+      />
+
+      <a href={qrcodeLink} download={`qrcode.png`}>Baixar QrCode</a>
     </div>
         
   )
