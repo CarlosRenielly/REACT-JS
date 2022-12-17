50a7f29c65c1425e94370e7d5ba1ce79be2ffb03
Carlos-Renielly <many1458@gmail.com>
2022-12-17 13:24:42 -0300

    configurando o loading e o page 404

--- Newprojeto/meusite/src/Pages/Home/index.js
+++ Newprojeto/meusite/src/Pages/Home/index.js
@@ -1,11 +1,14 @@
 import { useEffect, useState} from 'react';
 import api from '../../services/api';
+import {Link} from 'react-router-dom';
+import './home.css';
 
 // URL DA API: movie/now_playing?api_key=7f55d4ca94d28d60688e92a275175d62
 
 function Home(){
 
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] =useState (true);
 
   useEffect(()=>{
 
@@ -16,15 +19,41 @@ function Home(){
          language: "pt-BR",
          page: 1,
         }
-      })
-      console.log(response.data.results);
+      });
+      //console.log(response.data.results.slice(0,10));
+      setFilmes(response.data.results.slice(0,10));
+      setLoading(false);
     }
 
     loadFilmes();
   }, [])
+
+  
+  if(loading) {
+    return(
+      <div className='loading'>
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
+  
+  
+  
+  
+  
   return(
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className='container'>
+      <div className='lista-filmes'>
+        {filmes.map((filme)=>{
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}` } alt={filme.title} />
+              <Link  to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- Newprojeto/meusite/src/routes.js
+++ Newprojeto/meusite/src/routes.js
@@ -4,6 +4,8 @@ import Header from './Componets/Header';
 import Home from './Pages/Home'
 import Filme from './Pages/Filme'
 
+import Erro from './Pages/Erro';
+
 function RoutesApp(){
   return(
     <BrowserRouter>
@@ -11,6 +13,8 @@ function RoutesApp(){
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/filme/:id" element={ <Filme/> } />
+
+        <Route path="*" element={<Erro/>} />
       </Routes>
     </BrowserRouter>
   )
